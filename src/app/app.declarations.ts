44f70d1404bfeae54389ac1189adad0dc2039318
gund <malkevich.alex@gmail.com>
2017-02-10 18:03:49 +0400

    refactor(comp): Remove NotFound component

--- src/app/app.declarations.ts
+++ src/app/app.declarations.ts
@@ -1,5 +1,4 @@
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const APP_DECLARATIONS = [
-  NotFoundComponent
+
 ];
--- src/app/components/not-found/not-found.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-not-found',
-  template: '<section><h3>Error 404: Not found</h3></section>'
-})
-export class NotFoundComponent { }
