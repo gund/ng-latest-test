6429874f7c85ad2e8300d558823f337e12b1249b
gund <malkevich.alex@gmail.com>
2017-02-10 17:46:09 +0400

    build(webpack): Use NoEmitOnErrorsPlugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -18,7 +18,7 @@ const {
   DllPlugin,
   DllReferencePlugin,
   ProgressPlugin,
-  NoErrorsPlugin
+  NoEmitOnErrorsPlugin
 } = require('webpack');
 
 const CompressionPlugin = require('compression-webpack-plugin');
@@ -181,7 +181,7 @@ const commonConfig = function webpackConfig(): WebpackConfig {
 
   if (PROD) {
     config.plugins.push(
-      new NoErrorsPlugin(),
+      new NoEmitOnErrorsPlugin(),
       new UglifyJsPlugin({
         beautify: false,
         comments: false
