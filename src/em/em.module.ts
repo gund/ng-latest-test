7d5ee1c6b2dcd2645d68640c1dd64ff3e2258adb
gund <malkevich.alex@gmail.com>
2017-02-12 02:17:32 +0400

    style(em): Remove comment from Em module

--- src/em/em.module.ts
+++ src/em/em.module.ts
@@ -1,11 +1,3 @@
-/**
- * This module is the entry for your App when NOT using universal.
- *
- * Make sure to use the 3 constant APP_ imports so you don't have to keep
- * track of your root app dependencies here. Only import directly in this file if
- * there is something that is specific to the environment.
- */
-
 import { EM_DECLARATIONS } from './em.declarations';
 import { EM_ENTRY_COMPONENTS } from './em.entry-components';
 import { EM_IMPORTS } from './em.imports';
