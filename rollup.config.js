ffdcfe14c2a84f08b7e4b22c0366a27b451c6e5a
gund <malkevich.alex@gmail.com>
2017-02-11 16:38:02 +0400

    build(rollup): Make globals configurable

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,21 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 
+function fromKebabToCamelCase(str) {
+  return str.replace(/\-(.{1})/g, (_, w) => w.toUpperCase());
+}
+
+const ngTester = /^\@angular\/(.+)/;
+ngTester.transform = fromKebabToCamelCase;
+
+const globalConf = {
+  'ng.$1': ngTester,
+  'ngBootstrap': '@ng-bootstrap\/ng-bootstrap',
+  'tslib': 'tslib',
+  'Rx': /^rxjs\/[^/]+$/,
+  'Rx.Observable': /^rxjs\/add\/observable\/[^/]+$/,
+  'Rx.Observable.prototype': /^rxjs\/add\/operator\/[^/]+$/,
+};
+
 export default {
   entry: 'dist/em/index.js',
   dest: 'dist/bundles/em.umd.js',
@@ -8,21 +24,39 @@ export default {
   plugins: [
     nodeResolve({ jsnext: true, browser: true })
   ],
-  globals: {
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    '@angular/http': 'ng.http',
-    '@angular/platform-browser': 'ng.platformBrowser',
-    '@ng-bootstrap/ng-bootstrap': 'ngBootstrap',
-    'rxjs/Subject': 'Rx',
-    'rxjs/Observable': 'Rx',
-    'rxjs/add/observable/of': 'Rx.Observable',
-    'rxjs/add/observable/empty': 'Rx.Observable',
-    'rxjs/add/operator/switchMap': 'Rx.Observable.prototype',
-    'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-    'rxjs/add/operator/publishLast': 'Rx.Observable.prototype',
-    'rxjs/add/operator/catch': 'Rx.Observable.prototype',
-    'tslib': 'tslib'
+  globals: (moduleId) => {
+    let global = '';
+
+    const isGlobalResolved = Object.keys(globalConf)
+      .some(pattern => {
+        const globMatcher = globalConf[pattern];
+
+        if (typeof globMatcher === 'string' && moduleId === globMatcher) {
+          global = pattern;
+          return true;
+        } else if (globMatcher instanceof RegExp) {
+          const matches = moduleId.match(globMatcher);
+
+          if (matches) {
+            global = matches.reduce((p, match, i) => {
+              if (typeof globMatcher.transform === 'function') {
+                match = globMatcher.transform(match);
+              }
+              return p.replace(`$${i}`, match);
+            }, pattern);
+
+            return true;
+          }
+        }
+
+        return false;
+      });
+
+    if (!isGlobalResolved) {
+      console.warn(`Global not resolved for module '${moduleId}'`);
+    }
+
+    return global;
   },
   external: (moduleId) => {
     if (/^(\@angular|\@ng\-bootstrap|rxjs)\//.test(moduleId)) {
