f2844fe5b6d8eb5df36e343610730f785d158f02
gund <malkevich.alex@gmail.com>
2017-02-12 02:16:43 +0400

    refactor(rollup): Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,5 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
-
-function fromKebabToCamelCase(str) {
-  return str.replace(/\-(.{1})/g, (_, w) => w.toUpperCase());
-}
-
-function transformer(regex, ...transformers) {
-  if (transformers && transformers.length > 0) {
-    regex.transform = val => transformers.reduce((v, fn) => fn(v), val);
-  }
-  return regex;
-}
-
-const globalConf = {
-  'ng.$1': transformer(/^\@angular\/(.+)/, fromKebabToCamelCase),
-  'ngBootstrap': '@ng-bootstrap\/ng-bootstrap',
-  'tslib': 'tslib',
-  'Rx': /^rxjs\/[^/]+$/,
-  'Rx.Observable': /^rxjs\/add\/observable\/[^/]+$/,
-  'Rx.Observable.prototype': /^rxjs\/add\/operator\/[^/]+$/,
-};
+import { globalsRegex, GLOBAL } from 'rollup-globals-regex';
 
 export default {
   entry: 'dist/em/index.js',
@@ -28,40 +9,14 @@ export default {
   plugins: [
     nodeResolve({ jsnext: true, browser: true })
   ],
-  globals: (moduleId) => {
-    let global = '';
-
-    const isGlobalResolved = Object.keys(globalConf)
-      .some(pattern => {
-        const globMatcher = globalConf[pattern];
-
-        if (typeof globMatcher === 'string' && moduleId === globMatcher) {
-          global = pattern;
-          return true;
-        } else if (globMatcher instanceof RegExp) {
-          const matches = moduleId.match(globMatcher);
-
-          if (matches) {
-            global = matches.reduce((p, match, i) => {
-              if (typeof globMatcher.transform === 'function') {
-                match = globMatcher.transform(match, i);
-              }
-              return p.replace(`$${i}`, match);
-            }, pattern);
-
-            return true;
-          }
-        }
-
-        return false;
-      });
-
-    if (!isGlobalResolved) {
-      console.warn(`Global not resolved for module '${moduleId}'`);
-    }
-
-    return global;
-  },
+  globals: globalsRegex({
+    'ngBootstrap': '@ng-bootstrap\/ng-bootstrap',
+    'tslib': 'tslib',
+    [GLOBAL.NG2]: GLOBAL.NG2.TPL,
+    [GLOBAL.RX]: GLOBAL.RX.TPL,
+    [GLOBAL.RX_OBSERVABLE]: GLOBAL.RX_OBSERVABLE.TPL,
+    [GLOBAL.RX_OPERATOR]: GLOBAL.RX_OPERATOR.TPL,
+  }),
   external: (moduleId) => {
     if (/^(\@angular|\@ng\-bootstrap|rxjs)\//.test(moduleId)) {
       return true;
